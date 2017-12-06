20221181b95384c27b5dfc055a60817c75f047ea
urosantic <urosantic@hotmail.com>
2017-12-06 13:29:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/admin/week2day2hw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.week2day2hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
